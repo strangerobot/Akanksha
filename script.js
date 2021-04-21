0576e0c72e46fb5cddf98f957ee67c061e51632a
Yatharth <yatharth13091997@gmail.com>
2021-04-21 16:05:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var count=1;
+function preload() {
+  table = loadTable("http://p5js.org/reference/assets/mammals.csv", 'csv', 'header');
+
+}
+
+function setup() {
+  if(table!=null)
+  {
+    count=table.getRowCount();
+  }
+ print(count);
+
+;
+}
+
+function shownext()
+{
+
+var current=int(random(0,count));
+print(current);
+document.getElementById("text").innerHTML = table.getString(current, 1)
+
+}
