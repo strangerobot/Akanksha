0fcb466a77f23ac42a5fc4ed7e0b25a0ddee7265
Yatharth <yatharth13091997@gmail.com>
2021-04-22 00:13:11 +0530

    refine

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ function setup() {
   {
     count=table.getRowCount();
   }
+  var current=int(random(0,count));
  print(count);
+ document.getElementById("text").innerHTML = table.getString(current, 0);
 
 ;
 }
@@ -21,5 +23,22 @@ function shownext()
 var current=int(random(0,count));
 print(current);
 document.getElementById("text").innerHTML = table.getString(current, 0)
+document.getElementById("text").style.filter= "blur(0px)";
+document.getElementById("text").style.color="#58575fb3";
+
+}
+
+function blurtext()
+{
+  print("mouseover")
+document.getElementById("text").style.filter= "blur(1px)";
+
+}
+
+function blurtextmore()
+{
+  print("mouseover")
+document.getElementById("text").style.filter= "blur(6px)";
+document.getElementById("text").style.color="#ffffff69";
 
 }
